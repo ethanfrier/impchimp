376a93035a55b8a13c0d15a9466e1cf111ca9695
Ethan Frier <ethanfrier2@ethan.(none)>
2013-11-24 14:20:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Impchimp::Application.config.secret_key_base = 'dec6242b92c93af197f49496f37ba7de41eb390db7b3eea8c3c9bc9f7786698d2575ddb61386d999f830d312057357cb715955f739394a84261ee3e589ddd87d'
